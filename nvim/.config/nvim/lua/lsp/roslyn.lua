17557bb30bafb3166d0691fede86c4ef84fa6de0
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2026-07-14 23:40:40 +0200

    add default mapping to roslyn lsp

--- nvim/.config/nvim/lua/lsp/roslyn.lua
+++ nvim/.config/nvim/lua/lsp/roslyn.lua
@@ -1,5 +1,7 @@
+local shared = require("lsp.shared")
 
 vim.lsp.config("roslyn_ls", {
+    on_attach = shared.on_attach,
     cmd = { "roslyn-language-server", "--stdio" },
     filetypes = { "razor", "cs" },
 })
