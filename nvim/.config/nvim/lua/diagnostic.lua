ff4574b535b19e7b9b38ba37294e215738e7195f
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2026-08-25 23:25:26 +0200

    better diagnostic keymap

--- nvim/.config/nvim/lua/diagnostic.lua
+++ nvim/.config/nvim/lua/diagnostic.lua
@@ -1,8 +1,13 @@
 print("Loading diagnostic settings")
 vim.diagnostic.config({
-    virtual_text = true,           -- Show errors at the end of the line
+    float = true,
+    virtual_text = false,           -- Show errors at the end of the line
     signs = true,                  -- Show signs in the gutter (left side)
     underline = true,              -- Underline the actual code with the error
     update_in_insert = true,       -- Don't update while typing (waits for you to leave Insert mode)
     severity_sort = true,          -- Sort by severity (errors first)
+    virtual_lines = false,
 })
+
+local opt = { noremap = true, silent = true }
+vim.keymap.set('n', '<Tab>', function() vim.diagnostic.open_float() end, With_desc('Diagnostic window', opt))
